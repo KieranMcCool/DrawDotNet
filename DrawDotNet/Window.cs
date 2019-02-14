97d5ac99d0e0af5804fafb411bcce9ffc1dfe087
Kieran McCool <kieranmccool332@gmail.com>
2019-02-14 23:31:58 +0000

    Initial Commit

---
+++ DrawDotNet/Window.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using SDL2;
+
+namespace DrawDotNet
+{
+    public class Window
+    {
+        public Window()
+        {
+            if (SDL.SDL_WasInit(SDL.SDL_INIT_VIDEO) == 0)
+            {
+                SDL.SDL_Init(SDL.SDL_INIT_VIDEO);
+            }
+        }
+    }
+}
